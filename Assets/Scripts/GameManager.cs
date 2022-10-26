71a9060cbbd2646bc13563d826179dd13c887fc0
Mark Trepanier <marktrepanier.cajigas@gmail.com>
2022-10-26 16:37:54 -0400

    adds functionality to start button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -18,5 +19,8 @@ public class GameManager : MonoBehaviour
         Debug.Log("here i am");
     }
 
-    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
